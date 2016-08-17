5f10aafbef35ecb7b72093c38e4d202a7d34cb29
deepak0004 <deepak14036@iiitd.ac.in>
2016-08-18 01:07:55 +0530

    Final:D

--- app/src/main/java/com/example/lenovo/checking_prime/Checking_Prime.java
+++ app/src/main/java/com/example/lenovo/checking_prime/Checking_Prime.java
@@ -57,115 +57,98 @@ public class Checking_Prime extends AppCompatActivity
         Button deepcorrec = (Button)findViewById(R.id.correc);
         Button deepincorrec = (Button)findViewById(R.id.incorrec);
 
-        deepcorrec.setEnabled(true);
-        deepincorrec.setEnabled(true);
         deepcorrec.setOnClickListener( new Button.OnClickListener()
-                {
-                    public void onClick(View v)
-                    {
-                        Button deepincorrec = (Button)findViewById(R.id.incorrec);
-                        boolean flag = false;
-                        int i;
+                                       {
+                                           public void onClick(View v)
+                                           {
+                                               boolean flag = false;
+                                               int i;
 
-                        if( i1!=1 )
-                        {
-                            for(i=2; i<i1; i++)
-                            {
-                                if( (i1%i)==0 )
-                                {
-                                    flag = true;
-                                }
-                            }
+                                               if( i1!=1 )
+                                               {
+                                                   for(i=2; i<i1; i++)
+                                                   {
+                                                       if( (i1%i)==0 )
+                                                       {
+                                                           flag = true;
+                                                       }
+                                                   }
 
-                            if( !flag )
-                            {
-                                Toast.makeText(getApplicationContext(),"It is Correct!!", Toast.LENGTH_SHORT).show();
-                            }
-                            else
-                            {
-                                Toast.makeText(getApplicationContext(),"It is Incorrect!!", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        else
-                        {
-                            Toast.makeText(getApplicationContext(),"It is Incorrect!!", Toast.LENGTH_SHORT).show();
-                        }
-                        deepincorrec.setEnabled(false);
-                    }
-                }
+                                                   if( !flag )
+                                                   {
+                                                       Toast.makeText(getApplicationContext(),"It is Correct!!", Toast.LENGTH_SHORT).show();
+                                                   }
+                                                   else
+                                                   {
+                                                       Toast.makeText(getApplicationContext(),"It is Incorrect!!", Toast.LENGTH_SHORT).show();
+                                                   }
+                                               }
+                                               else
+                                               {
+                                                   Toast.makeText(getApplicationContext(),"It is Incorrect!!", Toast.LENGTH_SHORT).show();
+                                               }
+
+                                           }
+                                       }
         );
 
 
         deepincorrec.setOnClickListener(new Button.OnClickListener()
-                {
-                    public void onClick(View v)
-                    {
-                        Button deepcorrec = (Button)findViewById(R.id.correc);
-                        boolean flag = false;
-                        int i;
+                                        {
+                                            public void onClick(View v)
+                                            {
+                                                boolean flag = false;
+                                                int i;
 
-                        if( i1!=1 )
-                        {
-                            for(i=2; i<i1; i++)
-                            {
-                                if( (i1%i)==0 )
-                                {
-                                    flag = true;
-                                }
-                            }
+                                                if( i1!=1 )
+                                                {
+                                                    for(i=2; i<i1; i++)
+                                                    {
+                                                        if( (i1%i)==0 )
+                                                        {
+                                                            flag = true;
+                                                        }
+                                                    }
 
-                            if( !flag )
-                            {
-                                Toast.makeText(getApplicationContext(),"It is Incorrect!!", Toast.LENGTH_SHORT).show();
-                            }
-                            else
-                            {
-                                Toast.makeText(getApplicationContext(),"It is Correct!!", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        else
-                        {
-                            Toast.makeText(getApplicationContext(),"It is Correct!!", Toast.LENGTH_SHORT).show();
-                        }
-                        deepcorrec.setEnabled(false);
-                    }
-                }
+                                                    if( !flag )
+                                                    {
+                                                        Toast.makeText(getApplicationContext(),"It is Incorrect!!", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                    else
+                                                    {
+                                                        Toast.makeText(getApplicationContext(),"It is Correct!!", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    Toast.makeText(getApplicationContext(),"It is Correct!!", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        }
         );
 
         Button deepnex = (Button)findViewById(R.id.nex);
         deepnex.setOnClickListener(new Button.OnClickListener()
-                {
-                    public void onClick(View v)
-                    {
-                        Button deepcorrec = (Button)findViewById(R.id.correc);
-                        Button deepincorrec = (Button)findViewById(R.id.incorrec);
+                                   {
+                                       public void onClick(View v)
+                                       {
+                                           Random r = new Random();
+                                           i1 = r.nextInt(1001 - 1) + 1;
 
-                        Random r = new Random();
-                        i1 = r.nextInt(1001 - 1) + 1;
-
-                        TextView tv =(TextView)findViewById(R.id.texx);
-                        tv.setText("Is " + Integer.toString( i1 ) + " a prime no?" );
-
-                        deepcorrec.setEnabled(true);
-                        deepincorrec.setEnabled(true);
-                    }
-                }
+                                           TextView tv =(TextView)findViewById(R.id.texx);
+                                           tv.setText("Is " + Integer.toString( i1 ) + " a prime no?" );
+                                       }
+                                   }
         );
 
         Button exit2 = (Button)findViewById(R.id.exitt);
         exit2.setOnClickListener(new Button.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                Button deepcorrec = (Button)findViewById(R.id.correc);
-                Button deepincorrec = (Button)findViewById(R.id.incorrec);
-
-                startActivity( new Intent(getApplicationContext(), Checking_Primee.class) );
-
-                deepcorrec.setEnabled(true);
-                deepincorrec.setEnabled(true);
-            }
-        }
+                                 {
+                                     public void onClick(View v)
+                                     {
+                                         startActivity( new Intent(getApplicationContext(), Checking_Primee.class) );
+                                     }
+                                 }
         );
     }
 
